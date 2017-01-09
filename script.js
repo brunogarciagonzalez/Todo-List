fbc031d4855f75f82ba5fe3ec1cb5cff2fbad576
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2017-01-09 01:07:21 -0500

    Replace for-loops with forEach native method

--- script.js
+++ script.js
@@ -20,23 +20,20 @@ var todoList = {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
 
-    //Get number of completed todos.
-    for (var i = 0; i < totalTodos; i++) {
-      if (this.todos[i].completed === true) {
+    //Get number of completed todos:
+    this.todos.forEach(function(todo) {
+      if(todo.completed === true) {
         completedTodos++;
       }
-    }
-    //Case 1: If everything is true, make everything false.
-    if (completedTodos === totalTodos) {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
-      }
-    //Case 2: Otherwise, make everything true.
-    } else {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
-      }
-    }
+    });
+    this.todos.forEach(function(todo) {
+      //Case 1: if everything true make everything false:
+      if (completedTodos === totalTodos) {
+        todo.completed = false;
+      //Case 2: otherwise, make everything true:
+      } else
+      todo.completed = true;
+    });
   }
 
 };
@@ -77,9 +74,9 @@ var view = {
   displayTodos: function() {
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = "";
-    for (var i = 0; i < todoList.todos.length; i++) {
+
+    todoList.todos.forEach(function(todo, position) {
       var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
       var todoTextWithCompletion = "";
 
       if (todo.completed === true) {
@@ -88,11 +85,11 @@ var view = {
         todoTextWithCompletion = "[ ] " + todo.todoText;
       }
 
-      todoLi.id = i;
+      todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
-    }
+    }, this);
   },
   createDeleteButton: function(){
     var deleteButton = document.createElement("button");
